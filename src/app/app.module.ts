ecb23d8976a0775e1b8f42a9dfa70eb0db5d7fab
mshabab89 <mmshabab90@gmail.com>
2018-07-23 16:55:33 +0200

    Security implemented with authentication | TODO: create user and verify auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,12 +15,19 @@ import { AlarmListComponent } from './alarm-list/alarm-list.component';
 import { MapComponent } from './map/map.component';
 import { ChatComponent } from './chat/chat.component';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
+
+import { AuthService } from './services/auth.service';
+import { AuthguardService } from './services/security/authguard.service';
 
 const routeLists: Routes = [
-  {path: 'alarms', component: AlarmsComponent}
-]
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'alarms', component: AlarmsComponent, canActivate: [AuthguardService]}
+];
 
 @NgModule({
   declarations: [
@@ -29,18 +37,21 @@ const routeLists: Routes = [
     AlarmsComponent,
     AlarmListComponent,
     MapComponent,
-    ChatComponent
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseChatDemo),
+    AngularFireAuthModule,
     AngularFireDatabaseModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
-    RouterModule.forRoot(routeLists)
+    RouterModule.forRoot(routeLists),
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthService, AuthguardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/header-navigation/header-navigation.component.ts
+++ src/app/header-navigation/header-navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../services/auth.service';
 
 @Component({
   selector: 'app-header-navigation',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderNavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  logOut() {
+    this.authService.logout();
   }
 
 }
